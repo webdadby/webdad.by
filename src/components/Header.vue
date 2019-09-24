e4c43ab6a8e2e58078bb328f0f604cae59f9d35d
webdadby <acount2016.1600010699101@gmail.com>
2019-09-24 10:28:42 +0300

    Создана страница ит услгу

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,57 +1,44 @@
 <template>
-        <div class="container">
-            <div class="video">
-                <video autoplay loop class="fillWidth1">
-                    <source src="../assets/video/Slow_Typer.mp4" type="video/mp4"/>
-                    Your browser does not support the video tag. I suggest you upgrade your browser.
-                    <source src="../assets/video/Slow_Typer.webm" type="video/webm"/>
-                    Your browser does not support the video tag. I suggest you upgrade your browser.
-                </video>
-                <!-- <img src="../assets/video/Slow_Typer.jpg" alt=""> -->
-            </div>
-            <div class="content">
-                <div class="discription">
-                    <div class="margin-b-40">
-                        <h1 class="carousel-title">Digital-агентство</h1>
-                        <p style="color: white">Мы - агентство с опытом и необыкновенными людьми</p>
-                    </div>
-                    <el-row>
-                        <el-button type="warning" round>Оставить заявку</el-button>
-                    </el-row>
-                    
-                </div>
-            </div>
+    <div class="container img_header" data-parallax="scroll">
+        <div class="discription">
+	        <h1></h1>
+	        <p>Разработка програмного комплекса</p>
+            <el-button type="warning" round>Оставить заявку</el-button>
         </div>
-        
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Header'
-  }
+    
+}
 </script>
-
 <style scoped>
+.container{
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 50vh;
+    width: 100%;
+    height: 50%;
+    left: 0;
+    top: 0;
+}
+.img_header{
+    background: url(../assets/img/1920x1080/2.png) center center no-repeat;
+}
 h1{
-    font-size: 90px;
+    font-size: 50px;
     font-weight: 700;
     color: #fff;
     line-height: 1.1;
 }
-.container{
-    display: grid;
-    grid-template-columns: 1fr;
-}
-video{
-    width: 100%;
-}
-.content{
-    z-index: 1;
-}
 .discription{
     position: absolute;
-    top: 35%;
+    top: 20%;
     text-align: left;
     padding-left: 20%;
+}
+p{
+    color: #fff;
 }
 </style>
