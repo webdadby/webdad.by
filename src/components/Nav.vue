a14be4ce185932956369410b2e440b3c37d4d872
webdadby <acount2016.1600010699101@gmail.com>
2019-10-01 14:27:33 +0300

    navbar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
- <nav class="navbar" role="navigation">
-     <div class="container">
+    <nav class="navbar" role="navigation">
+        <div class="container mobile-menu">
             <div class="menu-container">
                 <div class="logo">
                     <a class="logo-wrap" href="/">
@@ -9,18 +9,22 @@
                     </a>
                 </div>
             </div>
-            <div class="menu-container">
-                <ul class="navbar-nav navbar-nav-right">
-                        <li class="nav-item"><a class="nav-item-child nav-item-hover active" href="/">Главная</a></li>
-                        <li class="nav-item"><a class="nav-item-child nav-item-hover" href="/product">Продукты</a>
+            <div class="menu-container menu-container-mobile">
+                <input type="checkbox" id="checkbox" class="mobile-menu__checkbox">
+                <label for="checkbox" class="mobile-menu__btn"><div class="mobile-menu__icon"></div></label>
+                <div class="menu-container mobile-menu__container">
+                    <ul class="navbar-nav right mobile-menu__list">
+                        <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover active" href="/">Главная</a></li>
+                        <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover" href="/product">Продукты</a>
                         </li>
-                        <li class="nav-item"><a class="nav-item-child nav-item-hover" href="/partners">Партнеры</a></li>
-                        <!-- <li class="nav-item"><a class="nav-item-child nav-item-hover" href="products.html">Вакансии</a>
+                        <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover" href="/partners">Партнеры</a></li>
+                        <!-- <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover" href="products.html">Вакансии</a>
                         </li>
-                        <li class="nav-item"><a class="nav-item-child nav-item-hover" href="faq.html">FAQ</a></li> -->
-                        <li class="nav-item"><a class="nav-item-child nav-item-hover" href="/contact">Контакты</a>
+                        <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover" href="faq.html">FAQ</a></li> -->
+                        <li class="nav-item mobile-menu__item"><a class="mobile-menu__link nav-item-child nav-item-hover" href="/contact">Контакты</a>
                         </li>
                     </ul>
+                </div>
             </div>
         </div>
     </nav>
@@ -28,6 +32,7 @@
 
 <script>
 export default {
+    
   name: 'Nav',
   data () {
     return {
@@ -37,7 +42,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .navbar{
     padding: 10px;
@@ -54,6 +58,31 @@ export default {
     transition-duration: .3s;
     transition-property: all;
     transition-timing-function: cubic-bezier(.7,1,.7,1);
+}
+
+.menu-container li {
+	margin: 0 5px 10px 0;
+	padding: 0;
+	list-style: none;
+	display: inline-block;
+	display:inline;
+}
+.menu-container a {
+	padding: 3px 12px;
+	text-decoration: none;
+	color:#E6A23C;
+	line-height: 100%;
+}
+.menu-container a:hover {
+	color: #000;
+}
+.menu-container .current a {
+	background: #999;
+	color: #fff;
+	border-radius: 3px;
+}
+.menu-container.right ul {
+	text-align: right;
 }
 .container{
     display: grid;
@@ -80,7 +109,129 @@ li a{
     max-width: 300px;
 }
 .logo-img-active {
-
     display: none;
+}
+#checkbox{
+    display: none;
+}
+
+@media (max-width: 990px) {
+    
+.navbar{
+    background: none;}
+.mobile-menu {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0 16px;
+  top: 0;
+  background-color: #0a0a0a;
+  left: 0;
+  right: 0;
+  height: 50px;
+  z-index: 9999999;
+}
+.mobile-menu__btn {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 30px;
+  cursor: pointer;
+  transition: .4s;
+  float: right;
+
+}
+.mobile-menu__icon {
+  display: block;
+  position: relative;
+  background: white;
+  width: 90%;
+  height: 4px;
+  transition: .4s;
+}
+.mobile-menu__icon::after, .mobile-menu__icon::before {
+  content: "";
+  display: block;
+  position: absolute;
+  background: white;
+  width: 100%;
+  height: 4px;
+  transition: .4s;
+}
+.mobile-menu__icon::after {
+  top: 8px;
+}
+.mobile-menu__icon::before {
+  top: -8px;
+}
+.mobile-menu__container {
+  position: fixed;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  top: 50px;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  height: 0;
+  opacity: 1;
+  transition: .5s;
+  overflow: hidden;
+  background-color: #f6f8f7;
+}
+.mobile-menu__list {
+  transition: .5s;
+  list-style: none;
+  padding-left: 0;
+  margin-top: -150px;
+  display: grid;
+}
+.mobile-menu__item {
+  font-size: 26px;
+  padding-bottom: 15px;
+}
+.mobile-menu__link {
+  text-decoration: none;
+  color: #fff;
+}
+.mobile-menu__checkbox {
+  display: none;
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__nav {
+  opacity: 1;
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__container {
+  height: 100%;
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__btn .mobile-menu__icon {
+  background: transparent;
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__btn .mobile-menu__icon::before, .mobile-menu__checkbox:checked ~ .mobile-menu__btn .mobile-menu__icon::after {
+  top: 0;
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__btn .mobile-menu__icon::after {
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+.mobile-menu__checkbox:checked ~ .mobile-menu__btn .mobile-menu__icon::before {
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+.logo-img-main{
+    display: none;
+    
+}
+.logo-img-active {
+    max-width: 150px;
+    display: block;
+    float: left;
+}
+.menu-container-mobile{
+    width: 100%;
+}
 }
 </style>
